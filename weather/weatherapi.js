2e6ec5f292e306f29f486cb682e26a2540cd7e58
이세정 <qazmk567@gmail.com>
2012-09-22 22:21:13 +0900

    실시간 검색어 9초 업데이트, 메인 애니메이션 수정

--- weather/weatherapi.js
+++ weather/weatherapi.js
@@ -14,23 +14,28 @@
 			"mostlycloudy",
 			"rain",
 			"partlycloudy",
-			"/ig/images/weather/sunny.gif",
-			"/ig/images/weather/mist.gif",
-			"/ig/images/weather/fog.gif",
-			"/ig/images/weather/mostly_cloudy.gif",
-			"/ig/images/weather/mostly_cloudly.gif",
-			"/ig/images/weather/partly_cloudy.gif",
-			"/ig/images/weather/haze.gif",
-			"/ig/images/weather/smoke.gif",
-			"/ig/images/weather/flurries.gif",
-			"/ig/images/weather/chance_of_rain.gif",
-			"/ig/images/weather/mostly_sunny.gif",
-			"/ig/images/weather/chance_of_tstorm.gif",
-			"/ig/images/weather/sleet.gif",
-			"/ig/images/weather/snow.gif",
-			"/ig/images/weather/icy.gif",
-			"/ig/images/weather/dust.gif",
-			"/ig/images/weather/chance_of_storm.gif" ];
+			"clear",
+			"partlysunny",
+			"mostlysunny",
+			"fog",
+			"mostlycloudy",
+			"hazy",
+			"hazy",
+			"flurries",
+			"chancestorms",
+			"chancerain",
+			"mostlysunny",
+			"chancestorms",
+			"sleet",
+			"snow",
+			"sleet",
+			"hazy",
+			"sunny",
+			"chancestorms",
+			"partlysunny",
+			"chancesnow",
+			"chancesleet.gif",
+			"chanceflurries.gif", ];
 		disp_weather = [
 			'weather/weather_storm.png',
 			'weather/weather_cloudy.png',
@@ -38,8 +43,8 @@
 			'weather/weather_rain.png',
 			'weather/weather_cloudy.png',
 			'weather/weather_sunny.png',
-			'weather/weather_storm.png',
-			'weather/weather_storm.png',
+			'weather/wetaher_mostlycoludy.png',
+			'weather/wetaher_mostlysunny.png',
 			'weather/weather_fog.png',
 			'weather/weather_mostlycloudy.png',
 			'weather/weather_haze.png',
@@ -51,13 +56,19 @@
 			'weather/weather_snow.png',
 			'weather/weather_snow.png',
 			'weather/weather_snow.png',
-			'weather/weather_dust.png',
-			'weather/weather_chancestorm.png' ];
+			'weather_dust.png',
+			'weather_sunny.png',
+			'weather_chancestorm.png',
+			'weather_mostlycloudy.png',
+			'weather_chancesnow.png',
+			'weather_chancesnow.png',
+			'weather_icyrain.png' ];
 
 		for (i = 0; i < yahoo_weather.length; i++ )
 			if( today.icon == yahoo_weather[ i ] )
 				weathericon.src = disp_weather[ i ];
 
+	
 		$("#todaywindspeed").text(today.avewind.kph + " Km/H " );
 		$("#todayrained").text(today.qpf_allday.mm + " mm " );
 
@@ -78,7 +89,3 @@
 
 
   
-
-
-
-	
